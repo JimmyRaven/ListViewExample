67d25852cb5d17ce8ac513898a281b65bf500687
philip <1499752783@qq.com>
2019-11-21 08:25:40 +0800

    card done

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -51,22 +51,22 @@ public class MainActivity extends AppCompatActivity  {
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
     public ArrayList getPlayers() {
         ArrayList<Player> players = new ArrayList<Player>();
-        players.add(new Player("Cristiano Ronaldo", 34, 460000000, "football", R.drawable.CRonaldo, "https://en.wikipedia.org/wiki/Cristiano_Ronaldo"));
-        players.add(new Player("LeBron James", 34, 480000000, "basketball", R.drawable.LJames, "https://en.wikipedia.org/wiki/LeBron_James"));
-        players.add(new Player("Lionel Messi", 32, 400000000, "football", R.drawable.LMessi, "https://en.wikipedia.org/wiki/LeBron_James"));
-        players.add(new Player("Sun Yang", 27, 3000000, "swimming", R.drawable.SYang, "https://en.wikipedia.org/wiki/Sun_Yang"));
-        players.add(new Player("Usain Bolt", 33, 90000000, "track and field", R.drawable.UBolt, "https://en.wikipedia.org/wiki/Sun_Yang"));
-        players.add(new Player("Zhang Jike", 31, 1000000, "table tennis", R.drawable.ZJike, "https://en.wikipedia.org/wiki/Zhang_Jike"));
-        players.add(new Player("Serena Williams", 38, 200000000, "tennis", R.drawable.SWilliams, "https://en.wikipedia.org/wiki/Serena_Williams"));
-        players.add(new Player("Li Na", 37, 50000000, "tennis", R.drawable.LNa, "https://en.wikipedia.org/wiki/Li_Na"));
+        players.add(new Player("Cristiano Ronaldo", 34, 460000000, "football", R.drawable.cronaldo, "https://en.wikipedia.org/wiki/Cristiano_Ronaldo"));
+        players.add(new Player("LeBron James", 34, 480000000, "basketball", R.drawable.ljames, "https://en.wikipedia.org/wiki/LeBron_James"));
+        players.add(new Player("Lionel Messi", 32, 400000000, "football", R.drawable.lmessi, "https://en.wikipedia.org/wiki/LeBron_James"));
+        players.add(new Player("Sun Yang", 27, 3000000, "swimming", R.drawable.syang, "https://en.wikipedia.org/wiki/Sun_Yang"));
+        players.add(new Player("Usain Bolt", 33, 90000000, "track and field", R.drawable.ubolt, "https://en.wikipedia.org/wiki/Sun_Yang"));
+        players.add(new Player("Zhang Jike", 31, 1000000, "table tennis", R.drawable.zjike, "https://en.wikipedia.org/wiki/Zhang_Jike"));
+        players.add(new Player("Serena Williams", 38, 200000000, "tennis", R.drawable.swilliams, "https://en.wikipedia.org/wiki/Serena_Williams"));
+        players.add(new Player("Li Na", 37, 50000000, "tennis", R.drawable.lna, "https://en.wikipedia.org/wiki/Li_Na"));
         // Phillip, add another 7
-        players.add(new Player("Derrick Rose", 31, 2100000, "basketball", R.drawable.DRose, "https://en.wikipedia.org/wiki/Derrick_Rose"));
-        players.add(new Player("Aaron Rodgers", 35, 89300000, "American football", R.drawable.ARodgers, "https://en.wikipedia.org/wiki/Aaron_Rodgers"));
-        players.add(new Player("Canelo Alvarez", 29, 92000000, "boxing", R.drawable.CAlvarez, "https://en.wikipedia.org/wiki/Canelo_%C3%81lvarez"));
-        players.add(new Player("Mike Trout", 28, 50600000, "baseball", R.drawable.MTrout, "https://en.wikipedia.org/wiki/Mike_Trout"));
-        players.add(new Player("Conor McGregor", 31, 47000000, "MMA", R.drawable.CMcGregor, "https://en.wikipedia.org/wiki/Conor_McGregor"));
-        players.add(new Player("Stephen Curry", 31, 79800000, "basketball", R.drawable.Curry, "https://en.wikipedia.org/wiki/Stephen_Curry"));
-        players.add(new Player("Chris Paul", 34, 43800000, "basketball", R.drawable.CP, "https://en.wikipedia.org/wiki/Chris_Paul"));
+        players.add(new Player("Derrick Rose", 31, 2100000, "basketball", R.drawable.drose, "https://en.wikipedia.org/wiki/Derrick_Rose"));
+        players.add(new Player("Aaron Rodgers", 35, 89300000, "American football", R.drawable.arodgers, "https://en.wikipedia.org/wiki/Aaron_Rodgers"));
+        players.add(new Player("Canelo Alvarez", 29, 92000000, "boxing", R.drawable.calvarez, "https://en.wikipedia.org/wiki/Canelo_%C3%81lvarez"));
+        players.add(new Player("Mike Trout", 28, 50600000, "baseball", R.drawable.mtrout, "https://en.wikipedia.org/wiki/Mike_Trout"));
+        players.add(new Player("Conor McGregor", 31, 47000000, "MMA", R.drawable.cmcgregor, "https://en.wikipedia.org/wiki/Conor_McGregor"));
+        players.add(new Player("Stephen Curry", 31, 79800000, "basketball", R.drawable.curry, "https://en.wikipedia.org/wiki/Stephen_Curry"));
+        players.add(new Player("Chris Paul", 34, 43800000, "basketball", R.drawable.cp, "https://en.wikipedia.org/wiki/Chris_Paul"));
         return players;
     }
 
